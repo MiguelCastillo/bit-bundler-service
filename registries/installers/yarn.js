dab6ca11876765d754c4ab895f2ba4d9fb9e92b4
Miguel Castillo <mcastillo@symphono.com>
2016-11-24 08:05:00 -0500

    tweaked logic to be more specific about the location of the path where yarn is located

--- registries/installers/yarn.js
+++ registries/installers/yarn.js
@@ -1,13 +1,15 @@
 const exec = require("../../utils/exec");
 const packageInstallParser = require("../packageInstallParser");
+const path = require("path");
 
 class Yarn {
   install(packages, options) {
     var cwd = options.cwd || process.cwd;
     var env = options.env || process.env;
     var packageInstall = packageInstallParser(packages);
+    var yarnExec = path.resolve(process.cwd(), "./node_modules/.bin/yarn");
 
-    return exec("../../node_modules/.bin/yarn", ["add", ...packageInstall], { cwd, env }).then(() => packages);
+    return exec(yarnExec, ["add", ...packageInstall], { cwd, env }).then(() => packages);
   }
 }
 

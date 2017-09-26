7514afc615e384ef02324e919e2876ec68c68c90
Miguel Castillo <mcastillo@symphono.com>
2017-09-25 23:22:39 -0400

    made changes so that yarn works with cwd again

--- registries/installers/yarn.js
+++ registries/installers/yarn.js
@@ -4,12 +4,12 @@ const path = require("path");
 
 class Yarn {
   install(packages, options) {
-    var cwd = options.cwd || process.cwd;
+    var cwd = options.cwd || process.cwd();
     var env = options.env || process.env;
     var packageInstall = packageInstallParser(packages);
     var yarnExec = path.join(process.cwd(), "./node_modules/.bin/yarn");
 
-    return exec(yarnExec, ["add", ...packageInstall], { cwd, env }).then(() => packages);
+    return exec(yarnExec, ["add", ...packageInstall, "--cwd", "."], { cwd, env }).then(() => packages);
   }
 }
 
